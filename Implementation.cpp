d1099dc2082032999809a34758934d0a1d8f5f18
theopaid <theodosis_paid@yahoo.gr>
2018-01-31 20:54:55 +0200

    Added the Market Feature and txt files which contain information about our game content

--- Implementation.cpp
+++ Implementation.cpp
@@ -3,6 +3,8 @@
 #include <ctime>
 #include <cstdlib>
 #include <string>
+#include <list>
+#include <fstream>
 #include "Interface.h"
 
 using namespace std;
@@ -267,25 +269,52 @@ Grid::Grid(int _x, int _y, int _heroes_count)
             my_grid[i][j] = static_cast<square_type>(rand() % 3);
         }
     }
-    for (int i = 0; i < x; i++)
-    {
-        for (int j = 0; j < y; j++)
-        {
-            switch (my_grid[i][j])
-            {
-            case nonAccessible:
-                cout << "n";
-                break;
-            case market:
-                cout << "m";
-                break;
-            case common:
-                cout << "c";
-                break;
-            }
-        }
-        cout << endl;
+    displayMap();
+    cout << "Now the market is being created!" << endl;
+    // List of Weapons
+    ifstream WeaponsFile;
+    WeaponsFile.open("weapons.txt");
+    if(!WeaponsFile) {
+        cerr << "Unable to open file weapons.txt";
+        exit(1);
     }
+    WeaponNode w_struct;
+    while(WeaponsFile >> w_struct.name >> w_struct.price >> w_struct.base_level >> w_struct.damage >> w_struct.hands);
+        WeaponList.push_back(w_struct);
+    WeaponsFile.close();
+    // List of Armors
+    ifstream ArmorsFile;
+    ArmorsFile.open("armors.txt");
+    if(!ArmorsFile) {
+        cerr << "Unable to open file armors.txt";
+        exit(1);
+    }
+    ArmorNode a_struct;
+    while(ArmorsFile >> a_struct.name >> a_struct.price >> a_struct.base_level >> a_struct.dmg_reduction);
+        ArmorList.push_back(a_struct);
+    ArmorsFile.close();
+    // List of Potions
+    ifstream PotionsFile;
+    PotionsFile.open("potions.txt");
+    if(!PotionsFile) {
+        cerr << "Unable to open file potion.txt";
+        exit(1);
+    }
+    PotionsNode p_struct;
+    while(PotionsFile >> p_struct.name >> p_struct.price >> p_struct.base_level >> p_struct.boost);
+        PotionsList.push_back(p_struct);
+    PotionsFile.close();
+    // List of Spells
+    ifstream SpellsFile;
+    SpellsFile.open("spells.txt");
+    if(!SpellsFile) {
+        cerr << "Unable to open file spells.txt";
+        exit(1);
+    }
+    SpellsNode s_struct;
+    while(SpellsFile >> s_struct.name >> s_struct.price >> s_struct.base_level >> s_struct.damage_var >> s_struct.dmg_var >> s_struct.mana >> s_struct.reduction);
+        SpellsList.push_back(s_struct);
+    SpellsFile.close();
 }
 
 void Grid::displayMap()
@@ -338,6 +367,65 @@ void Grid::move(next_move my_move)
     case left_:
 
         break;
+    }
+}
+
+void Grid::show_market()
+{
+    cout "/////////////// MARKET ////////////////" << endl;
+    //Weapons
+    cout << "Weapons you can BUY: " << endl;
+    cout << " Name | Price | Level Requirement | Damage | How many hands " << endl;
+    for(list<WeaponNode>::iterator it=WeaponList.begin(); it != WeaponList.end(); ++it){
+        cout << (*it).name << " " << (*it).price << " " << (*it).base_level << " " << (*it).damage << " " << (*it).hands << endl;
+    }
+    //Armors
+    cout << "Armors you can BUY: " << endl;
+    cout << " Name | Price | Level Requirement | Damage Reduction " << endl;
+    for(list<ArmorNode>::iterator it=ArmorList.begin(); it != ArmorList.end(); ++it){
+        cout << (*it).name << " " << (*it).price << " " << (*it).base_level << " " << (*it).dmg_reduction << endl;
+    }
+    //Potions
+    int i = 0;
+    for(list<PotionsNode>::iterator it=PotionsList.begin(); it != PotionsList.end(); ++it){
+        if(i==0) {
+            cout << "Strength Potions you can BUY: " << endl;
+            cout << " Name | Price | Level Requirement | Strength Boost " << endl;
+        }
+        if (i == 5)
+        {
+            cout << "Dexterity Potions you can BUY: " << endl;
+            cout << " Name | Price | Level Requirement | Dexterity Boost " << endl;
+        }
+        if (i == 10)
+        {
+            cout << "Agility Potions you can BUY: " << endl;
+            cout << " Name | Price | Level Requirement | Agility Boost " << endl;
+        }
+        cout << (*it).name << " " << (*it).price << " " << (*it).base_level << " " << (*it).boost << endl;
+        i++;
+    }
+    //Spells
+    i = 0;
+    for (list<SpellsNode>::iterator it = SpellsList.begin(); it != SpellsList.end(); ++it)
+    {
+        if (i == 0)
+        {
+            cout << "Ice Spells you can BUY: " << endl;
+            cout << " Name | Price | Level Requirement | Damage Variety | Mana | Damage Variety Reduction " << endl;
+        }
+        if (i == 5)
+        {
+            cout << "Fire Spells you can BUY: " << endl;
+            cout << " Name | Price | Level Requirement | Damage Variety | Mana | Armor Reduction" << endl;
+        }
+        if (i == 10)
+        {
+            cout << "Lighting Spells you can BUY: " << endl;
+            cout << " Name | Price | Level Requirement | Damage Variety | Mana | Dodge Possibility Reduction" << endl;
+        }
+        cout << (*it).name << " " << (*it).price << " " << (*it).base_level << " " << (*it).dmg_var << " " << (*it).mana << " " << (*it).reduction << endl;
+        i++;
     }
 }
 
